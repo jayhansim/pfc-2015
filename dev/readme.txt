34c1172119127cd655a0eb4e23ffae804087c04b
Jayhan Sim <jayhan2003@gmail.com>
2015-10-05 00:35:30 +0800

    first commit

---
+++ dev/readme.txt
@@ -0,0 +1,5 @@
+READ ME
+
+This theme was develop with Codekit app (https://incident57.com/codekit/).
+
+If you don't have Codekit, you will need to use task runner tools like Grunt or Gulp.
---
+++ footer.php
@@ -0,0 +1,79 @@
+				<section id="partner" class="partner">
+					<div class="container">
+						<div class="row">
+							<div class="grid text-center">
+
+								<p>Partner Logo</p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			</main>
+
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+				<div class="container">
+					<div class="row">
+						<div class="grid-1-4">
+							<h5>Courses</h5>
+							<ul>
+								<li>-	NASM CPT Certification</li>
+								<li>-	Power Plate CPPT Certification</li>
+								<li>-	ViPR Certification</li>
+								<li>-	Rumble Roller </li>
+								<li>-	CPR Certification </li>
+							</ul>
+						</div>
+						<div class="grid-1-4">
+							<h5>PFC</h5>
+							<ul><li>-	Personal Training</li>
+							<li>-	Group and Corporate Training</li>
+							<li>- About Us</li>
+							<li>- Contact Us</li></ul>
+
+						</div>
+						<div class="grid-1-4">
+							<h5>Connect</h5>
+							<ul>
+								<li>Facebook</li>
+								<li>Twitter</li>
+								<li>Instagram</li>
+							</ul>
+						</div>
+						<div class="grid-1-4">
+							<h5>Quick Contact</h5>
+							Address, phone and email
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="container">
+							<div class="grid">
+								<!-- copyright -->
+								<p class="copyright">
+									&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
+									<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</footer>
+			<!-- /footer -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
