5f79de6f510ec0dcb59c2d596fc7f03220304d58
Jayhan Sim <jayhan2003@gmail.com>
2015-10-17 14:02:10 +0800

    theming update - index.php

--- functions.php
+++ functions.php
@@ -256,7 +256,9 @@ function html5wp_pagination()
         'base' => str_replace($big, '%#%', get_pagenum_link($big)),
         'format' => '?paged=%#%',
         'current' => max(1, get_query_var('paged')),
-        'total' => $wp_query->max_num_pages
+        'total' => $wp_query->max_num_pages,
+        'prev_text'  => __('< Previous'),
+        'next_text'  => __('Next >')
     ));
 }
 
--- searchform.php
+++ searchform.php
@@ -1,6 +1,12 @@
-<!-- search -->
-<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-	<input class="search-input" type="search" name="s" placeholder="<?php _e( 'To search, type and hit enter.', 'html5blank' ); ?>">
-	<button class="search-submit" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
+<form method="get" id="searchform" role="search" action="<?php echo home_url(); ?>">
+  <div class="form-group form-search">
+    <input tabindex="-1" id="search" type="text" name="s" placeholder="Search news" class="form-control">
+    <button type="submit" class="btn btn-search">
+      <svg width="16" height="16" viewBox="0 0 16 16" xmls="http://www.w3.org/2000/svg">
+        <g>
+          <path d="M9.477 10.89C8.497 11.59 7.297 12 6 12c-3.314 0-6-2.686-6-6s2.686-6 6-6 6 2.686 6 6c0 1.296-.41 2.495-1.11 3.476l4.795 4.787c.39.39.39 1.024 0 1.414-.39.39-1.024.39-1.414 0L9.478 10.89zM6 10c2.21 0 4-1.79 4-4S8.21 2 6 2 2 3.79 2 6s1.79 4 4 4z"></path>
+        </g>
+      </svg>
+    </button>
+  </div>
 </form>
-<!-- /search -->
